145866e6fe6b7a67c79ebf2ac42975defc34467b
DShelef <88281521+DShelef@users.noreply.github.com>
2021-09-23 20:33:24 +0300

    Update test_openssh_vulns.py
    (my) name error

--- test_openssh_vulns.py
+++ test_openssh_vulns.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2021 Dan Shelf
+Copyright (c) 2021 Dan Shelef
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
